21600d22de2ad6e55d5048ea3a2acf8eaa56c185
Hilton <hilton@asia.cybridge.jp>
2020-07-22 10:44:24 +0900

    first commit

---
+++ resources/views/emails/qrcode.blade.php
@@ -0,0 +1,10 @@
+@component('mail::message')
+# Welcome to our world
+
+this is your qr code please do not share this code to anyone
+dsfgdsrfg
+
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
